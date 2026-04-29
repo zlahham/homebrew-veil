157b328f5127a638bcb9827d514924470b180849
Zaid Lahham <zaid@lahham.tech>
2026-04-29 08:07:01 +0300

    Bump veil to 1.0.5

--- Casks/veil.rb
+++ Casks/veil.rb
@@ -1,6 +1,6 @@
 cask "veil" do
-  version "1.0.4"
-  sha256 "762d71fdb29f8c4934537b5fb97ee201ff0f543ef07995b5704546354c7daf19"
+  version "1.0.5"
+  sha256 "61501efd84f009bf47c4778863e907f322f89ef2c853381187aeb5aee642a10f"
 
   url "https://github.com/zlahham/Veil/releases/download/v#{version}/Veil.zip"
   name "Veil"
